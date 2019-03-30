016ee4e6623862b71870f0c2f76ebacaafa0ef2e
louieogrady <l.ogrady@gmail.com>
2019-03-30 15:55:10 +0000

    fin

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+let element = document.getElementById('main')
+
+element.remove()
+
+var newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerText = 'Louie is the champion'
